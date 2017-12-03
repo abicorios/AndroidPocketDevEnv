4dce4cdb24dfa01fe2475a75185b93ad7634f936
abicorios <33332953+abicorios@users.noreply.github.com>
2017-12-03 23:55:43 +0200

    Update AndroidPocketDevEnv.sh

--- AndroidPocketDevEnv.sh
+++ AndroidPocketDevEnv.sh
@@ -3,7 +3,7 @@ cd
 echo 'cd
 set -o vi'>>.bash_profile
 pacman -Syu base-devel cmake python vim
-ln -s "/usr/lib/libncursesw.so.6.0" -T "/usr/lib/libtinfo.so.5"
+#ln -s "/usr/lib/libncursesw.so.6.0" -T "/usr/lib/libtinfo.so.5"
 echo "if empty(glob('~/.vim/autoload/plug.vim'))
   silent !curl -fLo ~/.vim/autoload/plug.vim --create-dirs
     \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
