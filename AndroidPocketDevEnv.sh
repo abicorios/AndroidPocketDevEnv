a56bab861cca1367b14ba4b9154419f19e1eac78
abicorios <33332953+abicorios@users.noreply.github.com>
2017-12-03 18:52:36 +0200

    Create AndroidPocketDevEnv.sh

---
+++ AndroidPocketDevEnv.sh
@@ -0,0 +1,12 @@
+cd
+pacman -S vim git base-devenv
+ln -s "/usr/lib/libncursesw.so.6.0" -T "/usr/lib/libtinfo.so.5"
+echo "if empty(glob('~/.vim/autoload/plug.vim'))
+  silent !curl -fLo ~/.vim/autoload/plug.vim --create-dirs
+    \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+endif
+call plug#begin('~/.vim/plugged')
+Plug 'Valloric/YouCompleteMe', { 'do': './install.py --clang-complete' }
+call plug#end()
+" >> .vimrc
