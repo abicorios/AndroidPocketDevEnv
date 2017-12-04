a8853608d36c91ae27b53a2cf1497a09b7d73114
abicorios <33332953+abicorios@users.noreply.github.com>
2017-12-04 03:09:52 +0200

    Update AndroidPocketDevEnv.sh

--- AndroidPocketDevEnv.sh
+++ AndroidPocketDevEnv.sh
@@ -2,7 +2,7 @@
 cd
 echo 'cd
 set -o vi'>>.bash_profile
-pacman -Syu base-devel cmake python vim
+pacman -Syu base-devel cmake python clang vim
 #ln -s "/usr/lib/libncursesw.so.6.0" -T "/usr/lib/libtinfo.so.5"
 echo "if empty(glob('~/.vim/autoload/plug.vim'))
   silent !curl -fLo ~/.vim/autoload/plug.vim --create-dirs
@@ -10,7 +10,7 @@ echo "if empty(glob('~/.vim/autoload/plug.vim'))
   autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
 endif
 call plug#begin('~/.vim/plugged')
-Plug 'Valloric/YouCompleteMe', { 'do': './install.py --clang-complete' }
+Plug 'Valloric/YouCompleteMe', { 'do': './install.py --clang-complete --system-libclang' }
 call plug#end()
 " >> .vimrc
 vim -c q
